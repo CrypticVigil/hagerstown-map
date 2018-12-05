d44e76be9d3d4f0f113abe847fb5ccb7267ceb15
chadbarrows <chad.m.barrows@gmail.com>
2018-12-04 19:12:10 -0500

    initial commit - dec04

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Sidebar extends Component {
+
+	render() {
+		return <div className='sidebar'>
+			<p>This is my sample text.</p>
+		</div>
+	}
+}
+
+export default Sidebar;
