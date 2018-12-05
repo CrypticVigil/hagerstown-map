b4e353cb8d30b220b89c2d66587eecdc5d0868f3
chadbarrows <chad.m.barrows@gmail.com>
2018-12-04 19:25:26 -0500

    added credentials and locations

---
+++ src/data/credentials.js
@@ -0,0 +1,6 @@
+//  Google Maps API key
+export const mapsKey = 'AIzaSyA-5Vwx42DwuoHH7CqTVMFtaJYCAqD9J3s';
+
+//  Foursquare API key and secret
+export const clientID = 'DMI2YE0INDUQ3LVK3F0VNB5ZCCPWIVUTJR3LVBVGU40D3TQZ';
+export const clientSecret = 'MXASP03WTKJB5HDK4N0TU011RFCTUQPSGKI3RE40INMD1ZRD';
---
+++ src/data/locations.js
@@ -0,0 +1,71 @@
+export const locationList = [{
+	location: {
+	  lat: 39.613917,
+	  lng: -77.693224
+	},
+	title: "Martins' Food"
+  },
+  {
+	location: {
+	  lat: 39.616126,
+	  lng: -77.670884
+	},
+	title: 'Washington County Regional Park'
+  },
+  {
+	location: {
+	  lat: 39.612243,
+	  lng: -77.723208
+	},
+	title: 'Washington Chicken'
+  },
+  {
+	location: {
+	  lat: 39.622017,
+	  lng: -77.696621
+	},
+	title: 'Covenant Life Church'
+	},
+  {
+	location: {
+	  lat: 39.614883,
+	  lng: -77.691030
+	},
+	title: "Little Caesars Pizza"
+  },
+  {
+	location: {
+	  lat: 39.609609,
+	  lng: -77.707001
+	},
+	title: 'Animal Health Clinic of Funkstown'
+	},
+  {
+	location: {
+	  lat: 39.599649,
+	  lng: -77.733352
+	},
+	title: 'Walmart Supercenter'
+  },
+  {
+	location: {
+	  lat: 39.626465,
+	  lng: -77.729655
+	},
+	title: "Krumpe's Do-Nut Shop"
+  },
+  {
+	location: {
+	  lat: 39.608367,
+	  lng: -77.733840
+	},
+	title: 'Hagerstown Premium Outlets'
+  },
+  {
+	location: {
+	  lat: 39.626885,
+	  lng: -77.698119
+	},
+	title: "Turner's Bowling Alley"
+  }
+];
